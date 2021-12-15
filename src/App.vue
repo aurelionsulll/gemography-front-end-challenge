d2b451d1e69f69f39e0455105674ab9988c86bde
mehdi <air@mehdi.local>
2021-12-15 19:05:59 +0100

    adding pagination

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,18 @@
 			<div
 				class="w-full max-w-3xl mx-auto bg-white shadow-lg rounded-sm border border-gray-200"
 			>
-				<header class="px-5 py-4 border-b border-gray-100">
+				<header class="px-5 py-4 border-b border-gray-100 flex justify-between">
 					<h2 class="font-semibold text-gray-800">Most starred Github repos</h2>
+					<ul class="space-x-2">
+						<li
+							v-for="page in 10"
+							:key="page"
+							class="inline-block border border-gray-600 py-0 px-1 rounded"
+							:class="currentPage == page ? 'bg-gray-600 text-white' : ''"
+						>
+							<button @click="changePage(page)">{{ page }}</button>
+						</li>
+					</ul>
 				</header>
 				<div class="p-3">
 					<div v-for="repo in repos" :key="repo">
@@ -33,6 +43,7 @@
 		data() {
 			return {
 				repos: {},
+				currentPage: 1,
 			};
 		},
 
@@ -40,12 +51,16 @@
 			getRepos() {
 				axios.get(
 					'https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc&page=' +
-						2
+						this.currentPage
 				)
 					.then((res) => {
 						this.repos = res.data.items;
 					})
 					.catch((error) => console.log(error));
+			},
+			changePage(page) {
+				this.currentPage = page;
+				this.getRepos();
 			},
 		},
 	};
