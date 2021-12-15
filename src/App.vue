3f198b3de56088ba83b63238a74f446fb9198072
mehdi <air@mehdi.local>
2021-12-15 17:42:46 +0100

    getting results from github api

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,28 @@
 </template>
 
 <script>
+  import axios from 'axios'
+
+  export default {
+
+    created() {
+      this.getRepos()
+    },
+
+    data() {
+      return {
+        repos: {}
+      }
+    },
+
+    methods: {
+      getRepos() {
+        axios.get('https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc')
+          .then((res) => console.log(res.data.items))
+          .catch( (error) => console.log(error))
+      }
+    }
+  }
 
 </script>
 
