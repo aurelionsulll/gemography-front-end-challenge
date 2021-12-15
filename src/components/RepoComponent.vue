1ce9e918a09fe4d4e0577f91e491174c61b69ec6
mehdi <air@mehdi.local>
2021-12-15 19:11:28 +0100

    image alt with repo owner name

--- src/components/RepoComponent.vue
+++ src/components/RepoComponent.vue
@@ -1,8 +1,12 @@
 <template>
 	<div class="flex space-x-3 items-center">
-		<img class="rounded w-28 h-28" :src="`${repo.owner.avatar_url}`" alt="repo name" />
+		<img
+			class="rounded w-28 h-28"
+			:src="`${repo.owner.avatar_url}`"
+			:alt="`${repo.owner.login}`"
+		/>
 		<div class="space-y-3">
-			<h2>{{ repo.name }}</h2>
+			<h2 class="font-bold text-lg">{{ repo.name }}</h2>
 			<p class="">
 				{{ repo.description }}
 			</p>
@@ -14,13 +18,13 @@
 					>
 					<span
 						class="border border-gray-600 px-1 py-1 rounded"
-						v-if="repo.open_issues_count"
+						v-show="repo.open_issues_count"
 						>Issues: {{ repo.open_issues_count }}</span
 					>
 				</div>
 				<p>
 					Submited {{ currentDateTime(repo.created_at) }}
-					{{ repo.owner.login }}
+					<span class="font-bold">{{ repo.owner.login }}</span>
 				</p>
 			</div>
 		</div>
