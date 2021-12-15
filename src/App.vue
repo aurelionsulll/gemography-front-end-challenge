dc35b32e1a31da82ed7b5ea07f4a8b6f11cb650e
mehdi <air@mehdi.local>
2021-12-15 17:59:18 +0100

    signle repo component ui

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,49 @@
 <template>
-  <h1 class="text-green-900">hello gemography</h1>
+	<section class="antialiased bg-gray-100 text-gray-600 h-screen px-4">
+		<div class="flex flex-col justify-center h-full">
+			<div
+				class="w-full max-w-2xl mx-auto bg-white shadow-lg rounded-sm border border-gray-200"
+			>
+				<header class="px-5 py-4 border-b border-gray-100">
+					<h2 class="font-semibold text-gray-800">Most starred Github repos</h2>
+				</header>
+				<div class="p-3">
+					<RepoComponent />
+				</div>
+			</div>
+		</div>
+	</section>
 </template>
 
 <script>
-  import axios from 'axios'
+	import axios from 'axios';
+	import RepoComponent from '@/components/RepoComponent';
 
-  export default {
+	export default {
+		component: {
+			RepoComponent,
+		},
 
-    created() {
-      this.getRepos()
-    },
+		created() {
+			// this.getRepos()
+		},
 
-    data() {
-      return {
-        repos: {}
-      }
-    },
+		data() {
+			return {
+				repos: {},
+			};
+		},
 
-    methods: {
-      getRepos() {
-        axios.get('https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc')
-          .then((res) => console.log(res.data.items))
-          .catch( (error) => console.log(error))
-      }
-    }
-  }
-
+		methods: {
+			getRepos() {
+				axios.get(
+					'https://api.github.com/search/repositories?q=created:>2017-10-22&sort=stars&order=desc'
+				)
+					.then((res) => console.log(res.data.items))
+					.catch((error) => console.log(error));
+			},
+		},
+	};
 </script>
 
-<style>
-
-</style>
+<style></style>
---
+++ src/components/RepoComponent.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="flex space-x-3 items-center">
+		<img
+			class="rounded w-28 h-28"
+			src="https://raw.githubusercontent.com/cruip/vuejs-admin-dashboard-template/main/src/images/user-36-05.jpg"
+			alt="repo name"
+		/>
+		<div class="space-y-3">
+			<h2>Repo name</h2>
+			<p class="">
+				Repo description Lorem ipsum dolor sit amet consectetur adipisicing elit.
+				Maiores temporibus, commodi odit nam neque
+			</p>
+
+			<div class="flex justify-between">
+				<div class="space-x-4">
+					<span class="border border-gray-600 px-1 py-1 rounded">Stars: 188k</span>
+					<span class="border border-gray-600 px-1 py-1 rounded">Issues: 188k</span>
+				</div>
+				<!-- momento impl -->
+				<p>Submited 30 days ago by repo name</p>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script></script>
+
+<style></style>
